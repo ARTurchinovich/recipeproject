b3d2903b28434fed6624aaefc66aa771f63d8199
ARTurchinovich <turchinovichar@gmail.com>
2020-11-08 01:15:13 +0300

    Added annotations, id, relations, lob to Notes and Recipe

---
+++ src/test/java/ru/turchinovich/recipeproject/RecipeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.turchinovich.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
